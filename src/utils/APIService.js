7b12dba440823bd867bcc4b41e76cff0243c576e
Abhilash Negi <tiger.abhi.3609@gmail.com>
2021-07-05 12:25:00 +0530

    cors 5

--- src/utils/APIService.js
+++ src/utils/APIService.js
@@ -8,7 +8,7 @@ const APIService = ({ apiRoute, method }) => {
     setIsLoading(true)
     fetch(`https://jsonkeeper.com/b/${apiRoute}`, {
       method: method,
-      mode: "cors",
+      mode: "no-cors",
     })
       .then((response) => {
         console.log("RESPONSE", response)
